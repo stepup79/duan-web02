a6485a3df65b969889eea811e95408b00d277a77
STUDENT <duythai6995@gmail.com>
2020-08-29 18:00:36 +0700

    cap nhat

---
+++ backend/pages/dashboard.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <!-- header -->
+    <?php include_once(__DIR__ . '/../layouts/partials/header.php'); ?>
+    <!-- End header -->
+
+
+    <!-- sidebar -->
+    <?php include_once(__DIR__ . '/../layouts/partials/sidebar.php'); ?>
+    <!-- End sidebar -->
+
+    <h2>NỘI DUNG</h2>
+
+    <!-- footer -->
+    <?php include_once(__DIR__ . '/../layouts/partials/footer.php'); ?>
+    <!-- End footer -->
+</body>
+</html>
